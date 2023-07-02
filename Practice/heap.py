b667c98f18341cd0c670a532f094f284d450737e
luphone04 <luphonemaw09@gmail.com>
2023-07-02 19:24:46 +0700

    Heap Practice

--- Practice/heap.py
+++ Practice/heap.py
@@ -1,22 +1,23 @@
 class heap:
-    def compare(x , y):
-        return x < y
-    
+    def compare(x, y):
+        x < y
     def empty(self):
         if self.heapsize == 0:
             return True
         else:
             return False
-        
+                        
+
     def heapify(self, i):
         l = i * 2 + 1
-        r = (i + 1) * 2
+        r = (i * 1) * 2
         if l < self.heapsize and self.cmp(self.a[l] , self.a[i]):
             largest = l
+
         else:
             largest = i
 
-        if r < self.heapsize and self.cmp(self.a[r] , self.a[i] ):
+        if r < self.heapsize and self.cmp(self.a[r] , self.a[i]):
             largest = r
 
         if largest != i:
@@ -24,40 +25,34 @@ class heap:
             self.heapify(largest)
 
 
-    def insert(self , x):
+
+
+    def insert(self, x):
         self.heapsize += 1
         if len(self.a) < self.heapsize:
             self.a.append(x)
         else:
             self.a[self.heapsize - 1] = x
-        i = self.heapsize - 1
-        j = (i - 1) //2
-        while i > 0 and self.cmp[self.a[i] ,self.a[j]]:
-            self.a[i] , self.a[j] = self.a[j], self.a[i]
-            i = j
-            j = (i - 1) //2
 
-        
     def extract(self):
         x = self.a[0]
-        last =self.heapsize - 1
+
+        last = self.heapsize - 1
         self.a[0] , self.a[last] = self.a[last] , self.a[0]
         self.heapsize -= 1
         self.heapify(0)
         return x
     
     def buildHeap(self):
-        for i in range((self.heapsize - 1)//2 , -1 , -1):
+        for i in range((self.heapsize - 1) // 2, -1 , -1):
             self.heapify(i)
 
-    
+
+
 
     def __init(self , items=[] , cmp = compare):
-        self.a= items
+        self.a = items
         self.cmp = cmp
-        self.heapsize = len(self.a)
+        self.heapsize = len(a)
         if len(self.a) > 0:
-            self.buildHeap()
-
-
-    
+            self.buildheap() 
