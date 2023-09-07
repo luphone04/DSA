985117295112033f6cc7dc503e4cdcb37f02a9a2
luphone04 <luphonemaw09@gmail.com>
2023-09-07 09:32:34 +0700

    PrepareWeek11

---
+++ Week11/connectedCheck-template.py
@@ -0,0 +1,13 @@
+V,E = map(int, input().split())
+edgeList = []
+for i in range(E):
+    edgeList.append(tuple(map(int, input().split())))
+
+from disjointsets3 import DisjointSets
+
+s = DisjointSets(V)
+
+# Complete the code below
+
+
+    
